a96edb579ec9732aeff8c36dadb19bb8cf414936
David Joshua Yopp <DavidJYopp@gmail.com>
2021-07-04 12:57:54 -0400

    added test for saving and retrieving file with len assertion

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected last card to be King of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 48 {
+		t.Errorf("Expected Desk Length of 48, but got %d", len(d))
+	}
+
+	os.Remove("_decktesting")
 }
